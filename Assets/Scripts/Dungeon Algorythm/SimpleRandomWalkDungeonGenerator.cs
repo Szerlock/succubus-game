fc27c042764c386a9ae21a97122b8d809701f9bc
Szerlock <ryan.cubas21@gmail.com>
2024-04-27 23:30:40 -0600

    procedural dungeon

---
+++ Assets/Scripts/Dungeon Algorythm/SimpleRandomWalkDungeonGenerator.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
+
+public class SimpleRandomWalkDungeonGenerator : AbstractDungeonGenerator
+{
+
+  [SerializeField]
+  private SimpleRandomData randonWalkParameters;
+
+  protected override void RunProceduralGeneration()
+  {
+    HashSet<Vector2Int> floorPositions = RunRandomWalk();
+    tilemapVisualizer.Clear();
+    tilemapVisualizer.PaintFloorTiles(floorPositions);
+  }
+
+  protected HashSet<Vector2Int> RunRandomWalk()
+  {
+    var currentPosition = startPosition;
+    HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
+    for(int i = 0; i < randonWalkParameters.iterations; i++)
+    {
+      var path = ProceduralGenerationAlgorithm.SimpleRandomWalk(currentPosition, randonWalkParameters.walkLength);
+      floorPositions.UnionWith(path);
+      if(randonWalkParameters.startRandomlyEachIteration)
+        currentPosition = floorPositions.ElementAt(Random.Range(0, floorPositions.Count));
+    }
+    return floorPositions;      
+  }
+}
